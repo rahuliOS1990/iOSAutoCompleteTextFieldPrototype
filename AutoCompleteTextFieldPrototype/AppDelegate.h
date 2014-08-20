cbda851685f6a04cfdc01a4121e2f135ac8a82ed
R. Sharma <rahul2.sharma@Mac132.local>
2014-08-20 13:38:57 +0530

    Initial Commit

---
+++ AutoCompleteTextFieldPrototype/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoCompleteTextFieldPrototype
+//
+//  Created by R. Sharma on 8/20/14.
+//  Copyright (c) 2014 AgileMobileDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoCompleteTextFieldPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoCompleteTextFieldPrototype
+//
+//  Created by R. Sharma on 8/20/14.
+//  Copyright (c) 2014 AgileMobileDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
